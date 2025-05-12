bfcefc447d4732f92df68eb4ff312de8497f3c90
davoodkhoshnood <khoshnood1977@gmail.com>
2025-05-12 19:59:55 +0100

    Add Book class

--- book-manager/src/main/java/com/davood/bookmanager/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.davood.bookmanager;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		Book myBook = new Book("The Hobbit", "J.R.R. Tolkien", 1937);
-		System.out.println(myBook);
-	}
-}
