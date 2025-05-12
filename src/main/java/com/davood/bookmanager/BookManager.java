fd74f41c30725933e7a368bed791187baeb72a9f
davoodkhoshnood <khoshnood1977@gmail.com>
2025-05-12 20:30:21 +0100

    Add BookManager to manage books

---
+++ src/main/java/com/davood/bookmanager/BookManager.java
@@ -0,0 +1,23 @@
+package com.davood.bookmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+    private List<Book> books;
+
+    public BookManager() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void listBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+}
+
--- src/main/java/com/davood/bookmanager/HelloWorld.java
+++ src/main/java/com/davood/bookmanager/HelloWorld.java
@@ -1,8 +1,14 @@
 package com.davood.bookmanager;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		Book myBook = new Book("The Hobbit", "J.R.R. Tolkien", 1937);
-		System.out.println(myBook);
-	}
+    public static void main(String[] args) {
+        BookManager manager = new BookManager();
+
+        manager.addBook(new Book("The Hobbit", "J.R.R. Tolkien", 1937));
+        manager.addBook(new Book("1984", "George Orwell", 1949));
+        manager.addBook(new Book("Clean Code", "Robert C. Martin", 2008));
+
+        manager.listBooks();
+    }
 }
+
