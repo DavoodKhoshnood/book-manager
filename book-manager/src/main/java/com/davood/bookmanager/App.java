9a585ebbcc37f314a24a7f360397cffaf121f940
davoodkhoshnood <khoshnood1977@gmail.com>
2025-05-12 19:18:40 +0100

    Setup Java project and Maven

---
+++ book-manager/src/main/java/com/davood/bookmanager/App.java
@@ -0,0 +1,13 @@
+package com.davood.bookmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
